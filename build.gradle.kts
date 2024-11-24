d498a9ab0177eb9087e7eef3978a22640f6cfaa5
JingMatrix <jingmatrix@gmail.com>
2024-11-24 19:18:50 +0100

    fix: trigger Zygisk modules service.sh in parallel (#91)
    This commit fixes 2 issues:
    - service.sh of files were not being executed in parallel, a behavior described in: https://github.com/topjohnwu/Magisk/blob/master/docs/guides.md#boot-scripts . This was fixed by using "&" in the end of the command to execute the module's service.sh, telling the shell to not block.
    - Gradle wrapper used in ReZygisk was old, causing failing CIs.
    
    It also updates the NDK version to ensure we'll be operating smoothly.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ val androidMinSdkVersion by extra(26)
 val androidTargetSdkVersion by extra(34)
 val androidCompileSdkVersion by extra(34)
 val androidBuildToolsVersion by extra("34.0.0")
-val androidCompileNdkVersion by extra("26.0.10792818")
+val androidCompileNdkVersion by extra("27.2.12479018")
 val androidSourceCompatibility by extra(JavaVersion.VERSION_11)
 val androidTargetCompatibility by extra(JavaVersion.VERSION_11)
 
